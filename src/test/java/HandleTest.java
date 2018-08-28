6bc7cd814133648c6418202f8652d639d34de5c2
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-08-28 18:13:06 +0200

    handle - naming

--- src/test/java/HandleTest.java
+++ src/test/java/HandleTest.java
@@ -3,6 +3,8 @@ import org.junit.Test;
 import java.util.concurrent.CompletableFuture;
 
 import static java.util.Objects.nonNull;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * Created by mtumilowicz on 2018-08-28.
@@ -11,16 +13,17 @@ public class HandleTest {
     @Test
     public void handle_exception() {
         CompletableFuture<String> handle = CompletableFuture.supplyAsync(ExceptionProcess::call)
-                .handle((r, t) -> nonNull(r) ? r : String.format("Task ended with exception: %s", t));
+                .handle((ok, ex) -> nonNull(ok) ? ok : String.format("Task ended with exception: %s", ex));
 
-        System.out.println(handle.join());
+        assertThat(handle.join(), is("Task ended with exception: " +
+                "java.util.concurrent.CompletionException: java.lang.RuntimeException"));
     }
     
     @Test
     public void handle_noException() {
         CompletableFuture<String> handle = CompletableFuture.supplyAsync(SlowProcess::call)
-                .handle((r, t) -> nonNull(r) ? r : String.format("Task ended with exception: %s", t));
+                .handle((ok, ex) -> nonNull(ok) ? ok : String.format("Task ended with exception: %s", ex));
 
-        System.out.println(handle.join());
+        assertThat(handle.join(), is("ended!"));
     }
 }
